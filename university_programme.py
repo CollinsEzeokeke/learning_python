0e0a040d9b0b450d57c07dcd23be301cf5cdbfe1
CollinsEzeokeke <90806850+CollinsEzeokeke@users.noreply.github.com>
2026-03-03 16:09:34 +0100

    feat: Add loan eligibility checks using logical operators and refactor university eligibility to use a ternary operator.

--- university_programme.py
+++ university_programme.py
@@ -1,9 +1,12 @@
 age = int(input("Enter your age: "))
 message = ""
-if age >= 18:
-    message = "You are eligible for university"
-else:
-    message = "You are not eligible for university"
-
+# if else statement
+# if age >= 18:
+#     message = "You are eligible for university"
+# else:
+#     message = "You are not eligible for university"
+# ternary operator
 message = "You are eligible for university" if age >= 18 else "You are not eligible for university"
 print(message)
+
+
