b18ee76016efac46f5982c094cb4fe4c10f9d90f
CollinsEzeokeke <90806850+CollinsEzeokeke@users.noreply.github.com>
2026-03-03 08:44:59 +0100

    feat: Add examples for escape sequences, string concatenation, and f-strings.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 course = "Python Programming"
+numberDefined = 12
 # tripple quotes stings
 message = """
 Hi John,
@@ -15,3 +16,24 @@ print(course[0:3])
 print(course[0:])
 print(course[:3])
 print(course[:])
+
+# escape sequences
+# \"
+# \n
+# \t
+# \r
+# \b
+# \f
+# \v
+# \\ 
+
+newCourse = "Python \nProgramming "
+print(newCourse)
+
+first = "John"
+last = "Doe"
+full_name = first + " " + last
+print(full_name)
+
+# formatted strings
+print(f"Hello there {first} {last} {numberDefined}")
