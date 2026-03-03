6d339b28999f1063b677e975f718a0483c4533ec
CollinsEzeokeke <90806850+CollinsEzeokeke@users.noreply.github.com>
2026-03-03 16:26:18 +0100

    feat: Add loan eligibility checks using boolean logic with `and`, `or`, and `not` operators.

--- university_programme.py
+++ university_programme.py
@@ -10,3 +10,35 @@ message = "You are eligible for university" if age >= 18 else "You are not eligi
 print(message)
 
 
+#
+highIncome = input("Is your income high? (True/False): ")
+goodCreditScore = input("Do you have a good credit score? (True/False): ")
+educationStatus = input("Do you have a good education status? (True/False): ")
+creditMessage = ""
+educationMessage = ""
+
+if highIncome.lower() == "true" and goodCreditScore.lower() == "true":
+    creditMessage = "Eligible for loan"
+else:
+    creditMessage = "Not eligible for loan"
+print(creditMessage + " creditMesssage from and")
+
+# if one is true and the other isn't
+if highIncome.lower() == "true" or goodCreditScore.lower() == "true":
+    creditMessage = "Eligible for loan"
+else:
+    creditMessage = "Not eligible for loan"
+print(creditMessage + " creditMesssage from or")
+
+if not educationStatus.lower() == "true":
+    educationMessage = "Eligible for loan"
+else:
+    educationMessage = "Not eligible for loan students aren't permitted to take on loans yet!"
+print(educationMessage + " educationMessage from not")
+
+
+# all used at once
+if highIncome.lower() == "true" and goodCreditScore.lower() == "true" and age >= 18 and (not educationStatus.lower() == "true"):
+    print("Business is booming in this time of the year let's get to work!")
+else:
+    print("Improve on your income, credit score, age, and education status to be eligible for a loan! and you're a student step up")
