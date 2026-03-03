95a631c3a27c788dbf82e63c058febc70bc0ab6d
CollinsEzeokeke <90806850+CollinsEzeokeke@users.noreply.github.com>
2026-03-03 03:13:09 +0100

    feat: Replace basic variable declarations with string manipulation examples including multi-line strings, length, and indexing.

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-students_count = 1000
-print(students_count)
+course = "Python Programming"
+# tripple quotes stings
+message = """
+Hi John,
 
-# float
-rating = 4.5
-print(rating)
-
-# boolean
-is_published = True
-print(is_published)
-
-course_name = "Python Programming"
-print(course_name)
-
-#  string
-studentsName = "Jack Ma"
-print(studentsName)
+This is a multi-line string.
+"""
+# len function for getting length of a string
+lenghtNumber = len(course)
+print(lenghtNumber)
+# indexing
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[0:])
+print(course[:3])
+print(course[:])
